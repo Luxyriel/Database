5ff5ab8a773c33cf2c2e1a22f5ce156bbc9bef60
Luxyriel <151851950+Luxyriel@users.noreply.github.com>
2023-12-12 20:06:37 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+- 👋 Hi, I’m @longthinh
+- 👀 I’m interested in Infomation Technology
+- 🌱 I’m currently learning HTML, JavaScript, Python, C/C++ etc
+- 💞️ I’m looking to collaborate on https://t.me/longthinh
+- 📫 How to reach me longthinh@mail.com
+- ⚠️ Warning! Do not copy in any form
+```
---
+++ Swift/Supervised.js
@@ -0,0 +1,26 @@
+/** @𝐋𝐮𝐱𝐲𝐫𝐢𝐞𝐥 𝕏 (longthinh) 2023-12-09 12:57:05 AM */
+
+var DIRECT = "DIRECT 127.0.0.1";
+var PROXY = "PROXY 127.0.0.1:80";
+var blacklist = {
+  // "gdmf.apple.com": 1,
+  // "mesu.apple.com": 1,
+  // "updates.cdn-apple.com": 1,
+  // "updates-http.cdn-apple.com": 1,
+  "su.itunes.apple.com": 1,
+};
+function FindProxyForURL(url, host) {
+  host = host.toLowerCase();
+  for (i = 0; i < 99; i++) {
+    if ((blacklist[host])) {
+      return PROXY;
+    }
+    var index = host.indexOf(".");
+    if (index == -1) {
+      break;
+    } else {
+      host = host.substring(index + 1);
+    }
+  }
+  return DIRECT;
+}
